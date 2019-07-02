d79a5e38167fb62cf4995f27406c519cba0709ac
Alekseev Yaroslav <alekseevy509@gmail.com>
2019-07-02 23:31:42 +0300

    Initial commit

---
+++ src/main/java/LockWithSyncronized/Difference.java
@@ -0,0 +1,36 @@
+package LockWithSyncronized;
+
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Difference {
+    private static final Object object1=new Object();
+    private static final Object object2=new Object();
+
+    private static Lock lock1=new ReentrantLock();
+    private static Lock lock2=new ReentrantLock();
+
+
+    public static void main(String[] args) {
+        synchronized (object1){
+            System.out.println("enter in first synch");
+            synchronized (object2){
+                System.out.println("enter in second synch");
+                System.out.println("out from second synch");
+            }
+            System.out.println("out from first synch");
+        }
+
+        //Lock
+
+
+        lock1.lock();
+        System.out.println("enter in first lock");
+        lock2.lock();
+        System.out.println("enter in second lock");
+        lock1.unlock();
+        System.out.println("out from first lock");
+        lock2.unlock();
+        System.out.println("out from second lock");
+    }
+}
